95ca9b1da8bad56b5b4dce59b5fd5bb046973ad7
pocketberserker <pocketberserker@gmail.com>
2018-01-12 22:56:07 +0900

    include akashic-new in release

--- release.sh
+++ release.sh
@@ -2,18 +2,27 @@
 
 set -e
 
-executable=akashic
+commands=(
+  akashic
+  akashic-new
+)
 
 rm -rf release
-gox -verbose -output "release/${executable}_{{.OS}}_{{.Arch}}" ./$executable
+for executable in "${commands[@]}"; do
+  gox -verbose -output "release/${executable}_{{.OS}}_{{.Arch}}" ./$executable
+done
 cd release
-for bin in *; do
-  if [[ "$bin" == *windows* ]]; then
-    command="${executable}.exe"
-  else
-    command="$executable"
-  fi
-  mv "$bin" "$command"
-  zip "${bin}.zip" "$command"
-  rm "$command"
+for bin in akashic_*; do
+  args=()
+  for executable in "${commands[@]}"; do
+    if [[ "$bin" == *windows* ]]; then
+      command="${executable}.exe"
+    else
+      command="${executable}"
+    fi
+    args+=($command)
+    mv "${bin/akashic/$executable}" "$command"
+  done
+  zip "${bin}.zip" ${args[*]}
+  rm ${args[*]}
 done
